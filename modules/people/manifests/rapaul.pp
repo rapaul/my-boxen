e6f083706bfbe7870697c56aafd128245e8ab22a
Richard Paul <richard.a.paul@gmail.com>
2013-09-14 13:33:16 +1200

    Added colour to git

--- modules/people/manifests/rapaul.pp
+++ modules/people/manifests/rapaul.pp
@@ -2,6 +2,9 @@ class people::rapaul {
   git::config::global { 'user.email':
     value  => 'richard.a.paul@gmail.com'
   }
+  git::config::global { 'color.ui':
+    value => 'true'
+  }
 
   include colloquy
 }
